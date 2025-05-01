0948834612884423340f2ec6f358c16ac3430a4e
Deniz Gunay <andacg1@hotmail.com>
2025-04-30 20:47:14 -0700

    feat(PC-8): added Product Specs page with basic text fields

--- product-comparison/app/routes/app.tsx
+++ product-comparison/app/routes/app.tsx
@@ -25,6 +25,7 @@ export default function App() {
           Home
         </Link>
         <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/metafields">Product Specs</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
