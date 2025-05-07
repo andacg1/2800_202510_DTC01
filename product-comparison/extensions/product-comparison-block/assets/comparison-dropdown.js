d9137b66723176014ffe2db1fa0bc09dd220f30e
Deniz Gunay <andacg1@hotmail.com>
2025-05-07 15:31:32 -0700

    feat(PC-52): Add a product specification table with a dropdown to select a product

--- product-comparison/extensions/product-comparison-block/assets/comparison-dropdown.js
+++ product-comparison/extensions/product-comparison-block/assets/comparison-dropdown.js
@@ -1,21 +1,30 @@
 //@ts-check
 window.addEventListener("load", (event) => {
-  window.location
+  const dropdownEl = document.getElementById("active-comparison");
+  dropdownEl.addEventListener("change", (e) => {
+    console.log({ newValue: e.target.value });
+    const handle = e.target.value;
+    const tableEl = document.getElementById("comparison-dropdown-table");
+    const elementsToHide = tableEl.querySelectorAll(
+      `td:not([data-handle='${handle}']),th:not([data-handle='${handle}'])`,
+    );
+    const elementsToShow = tableEl.querySelectorAll(
+      `td[data-handle='${handle}'],th[data-handle='${handle}']`,
+    );
+    const isCurrentProduct = (handle) =>
+      window.location.pathname.includes(handle);
 
-  const dropdownEl = document.getElementById('active-comparison');
-  dropdownEl.addEventListener('change', (e) => {
-    console.log({ newValue: e.target.value })
-    const handle = e.target.value
-    const tableEl = document.getElementById('comparison-dropdown-table');
-    const elementsToHide = tableEl.querySelectorAll(`td:not([data-handle='${handle}']),th:not([data-handle='${handle}'])`)
-    const elementsToShow = tableEl.querySelectorAll(`td[data-handle='${handle}'],th[data-handle='${handle}']`)
-    console.log({elementsToHide})
-    for (const element of elementsToHide) {
-      element.classList.add('hide')
-    }
+    console.log({ elementsToHide });
     for (const element of elementsToShow) {
-      element.classList.remove('hide')
+      element.classList.remove("hide");
+    }
+
+    for (const element of elementsToHide) {
+      const isFirstColumn = !element.dataset.handle;
+      if (isFirstColumn) {
+        continue;
+      }
+      element.classList.add("hide");
     }
   });
 });
-
