66fcae9f612f67452f942828402a4d5f85709cfd
Connor Brown <96628439+falconnor4@users.noreply.github.com>
2025-05-08 11:52:34 -0700

    feat(PC-84): dummy parcel building

--- product-comparison/frontend/src/index.tsx
+++ product-comparison/frontend/src/index.tsx
@@ -2,8 +2,9 @@ import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { App } from './App';
 
-let container = document.getElementById("app")!;
-let root = createRoot(container)
+// Target the container ID in react_demo.liquid
+let container = document.getElementById("container")!;
+let root = createRoot(container);
 root.render(
   <StrictMode>
     <App />
